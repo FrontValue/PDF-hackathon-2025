616053a29ea43b67bf2d013068376f60043128a5
Dejan Belušić <dejanbelusic@gmail.com>
2025-06-17 14:44:14 +0200

    add ip address

--- echo_server.py
+++ echo_server.py
@@ -1,14 +1,23 @@
-# echo_server.py
 import asyncio
 from websockets.asyncio.server import serve
+import socket
+
+async def print_ip():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return ip_address
 
 async def echo(websocket):
+    client_ip, client_port = websocket.remote_address
+    print(f"🔌 Client connected from {client_ip}:{client_port}")
+
     async for message in websocket:
-        await websocket.send(f"Echo: {message}")
+        await websocket.send(f"📦 Echo: {message}")
 
 async def main():
     async with serve(echo, "0.0.0.0", 8765):
-        print("Please be patient. You are being hacked! 🚀")
+        print("🚀 PDF - WebSocket connected!")
+        print(f"🗺️ WebSocket IP address: ws://{await print_ip()}:8765")
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
--- setup_websocket_server.sh
+++ setup_websocket_server.sh
@@ -4,14 +4,13 @@ sudo apt update && sudo apt upgrade -y
 echo "🐍 Installing Python3 and pip..."
 sudo apt install -y python3 python3-pip
 
-echo "🐍 Installing Virtual Environment..."
+echo "🐍 Setting up Virtual Environment..."
 python3 -m venv venv
 source venv/bin/activate
 
-echo "🌐 Installing websockets module..."
+echo "🌐 Installing modules..."
 pip install websockets
+pip install socket
 
 echo "📝 Starting WebSocket echo server..."
 python3 echo_server.py
-
-echo "🚀 WebSocket server is ready to receive connections on ws://<your-pi-ip>:8765.
