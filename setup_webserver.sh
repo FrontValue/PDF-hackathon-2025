88fd710f45ebbfba63f3e4b96b1be25f3e2a9965
Dejan Belušić <dejanbelusic@gmail.com>
2025-06-17 16:17:42 +0200

    flask server

--- setup_webserver.sh
+++ setup_webserver.sh
@@ -6,25 +6,12 @@ sudo apt update && sudo apt upgrade -y
 echo "🐍 Installing Python3 and pip..."
 sudo apt install -y python3 python3-pip
 
+echo "🐍 Setting up Virtual Environment..."
+python3 -m venv venv
+source venv/bin/activate
+
 echo "🌐 Installing Flask..."
-pip3 install Flask
-
-echo "📁 Creating project directory..."
-mkdir -p ~/pythonwebapp
-cd ~/pythonwebapp
-
-echo "📝 Creating Flask app..."
-cat <<EOF > app.py
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello from PDF team. You have been hacked! 🚀"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-EOF
+pip install Flask
 
 echo "🚀 Starting Flask server..."
 python3 app.py
